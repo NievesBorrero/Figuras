4a74c164d304806a2eecbeda65fca1ed17594e9d
Nieves Mª Borrero Barea <nievesborrerobarea@gmail.com>
2017-04-07 19:04:05 +0200

    Update DimensionNoValidaException.java

--- excepciones/DimensionNoValidaException.java
+++ excepciones/DimensionNoValidaException.java
@@ -1,4 +1,4 @@
-package exMarzo2017_NievesBorrero.excepciones;
+package Figuras.excepciones;
 
 public class DimensionNoValidaException extends Exception {
 	public DimensionNoValidaException(String msj){
